4fe0145d41cffa02098d0de99d2c1e3853edf1de
Meghana <saimeghanayerramsetty@gmail.com>
2024-10-21 11:20:02 +0530

    resolved signout error

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { useClerk, useUser, useOrganization } from "@clerk/nextjs";
+import { useUser, useOrganization } from "@clerk/nextjs";
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { useRouter } from 'next/navigation';
 import { Button } from "@/components/ui/button";
