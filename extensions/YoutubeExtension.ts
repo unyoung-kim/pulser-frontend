0545605542046eea1ac4ccb47b00716429e5d61a
Meghana <saimeghanayerramsetty@gmail.com>
2024-11-24 20:06:49 +0530

    build fixes

--- extensions/YoutubeExtension.ts
+++ extensions/YoutubeExtension.ts
@@ -1,13 +1,5 @@
 import Youtube from '@tiptap/extension-youtube'
 
-declare module '@tiptap/core' {
-  interface Commands<ReturnType> {
-    youtube: {
-      setYoutubeVideo: (options: { src: string, width?: number, height?: number }) => ReturnType
-    }
-  }
-}
-
 export const YoutubeExtension = Youtube.configure({
   inline: false,
   width: 640,
@@ -15,15 +7,5 @@ export const YoutubeExtension = Youtube.configure({
   allowFullscreen: true,
   controls: true,
   nocookie: false,
-  modestBranding: true,
-  addCommands() {
-    return {
-      setYoutubeVideo: (options) => ({ commands }) => {
-        return commands.setYoutubeVideo({
-          ...options,
-          src: options.src.startsWith('http') ? options.src : `https://www.youtube.com/watch?v=${options.src}`
-        })
-      }
-    }
-  }
+  modestBranding: true
 }) 
